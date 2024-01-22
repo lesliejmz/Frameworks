5a936848af09b6f87a62a18f95bfc70021741f59
lesliejmz <2202166@utrivieramaya.edu.mx>
2024-01-22 15:51:25 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'ut-color': '#EB5F46'
+      }
+    },
+  },
+  plugins: [],
+}
+
